1f5a15153904b9df24d56fb10d43a21a6f8b615c
mfuechec <fuechec.mark@gmail.com>
2020-11-17 14:56:38 -0600

    updated intro text

--- client/components/LandingPage/Introduction.jsx
+++ client/components/LandingPage/Introduction.jsx
@@ -4,8 +4,8 @@ export default function Introduction() {
     return (
         <div id='landingDescriptionBlock'>
             <p>Studies show that obesity has boomed in popularity in recent years and Cancel My Gym Membership is here to help you get in on this growing trend.</p>
-            <p>Unfortunately, cancelling a gym membership is usually more difficult than it needs to be. Many gyms won't allow you to cancel over the phone, and if you were willing to go into a gym you wouldn't be trying to cancel.</p>
-            <p>Luckily, many gyms offer other options. This website will help you find what works for you and, if possible, even send a letter or fax on your behalf.</p>
+            <p>Unfortunately, cancelling a gym membership is usually more difficult than it needs to be. Many gyms won't allow you to cancel over the phone, and someone willing to go into a gym wouldn't be trying to cancel.</p>
+            <p>Luckily, many gyms offer other options. This website will help you find what works for you and, if possible, offers the option of sending a letter on your behalf.</p>
         </div>
     )
 }
